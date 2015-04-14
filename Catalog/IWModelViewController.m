9be7abbb7cb63836849ee5bb5f3946404d5f862d
Abel Miranda <abel.miranda@globant.com>
2015-04-14 10:20:12 -0300

    Precios: Old Oak y Smoke Oak

--- Catalog/IWModelViewController.m
+++ Catalog/IWModelViewController.m
@@ -532,8 +532,8 @@
     [tableDimensionsView setText:table.size.name];
     [chairNameView setText:chair.model.name];
     NSString *imageName = [[[NSString stringWithFormat:@"%@-%@ ", table.model.name, [table.size.name stringByReplacingOccurrencesOfString:@" " withString:@""]] stringByReplacingOccurrencesOfString:@"/" withString:@" "] lowercaseString];
-    [tableTopSize setImage:[UIImage imageNamed:[imageName stringByAppendingString:@"p.png"]]];
-    [tableFrontSize setImage:[UIImage imageNamed:[imageName stringByAppendingString:@"a.png"]]];
+    [tableTopSize setImage:[UIImage imageNamed:[imageName stringByAppendingString:@"p"]]];
+    [tableFrontSize setImage:[UIImage imageNamed:[imageName stringByAppendingString:@"a"]]];
     [bottonDescriptionView setText:[NSString stringWithFormat:@"(*) Photo table sizes: %@", table.size.name]];
     [self updatePrices];
 }
