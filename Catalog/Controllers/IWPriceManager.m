f105050607668327e71e897a6769362a285568a3
Alejandro Molina Quiroga <alejandro.molina@Globants-MacBook-5.local>
2015-07-02 09:38:53 -0300

    Commit test

--- Catalog/Controllers/IWPriceManager.m
+++ Catalog/Controllers/IWPriceManager.m
@@ -2,7 +2,7 @@
 //  IWPriceManager.m
 //  Catalog
 //
-//  Created by Abel Miranda on 2/26/15.
+//  Created by Abel Miranda on 2/26/15..
 //  Copyright (c) 2015 Indian Webs. All rights reserved.
 //
 
